398f4d46817d030bcfdf430715ce4a048520e709
larangogon <larangogon1@uniminuto.edu.co>
2020-10-04 20:41:57 -0500

    style: se organiza codigo para que se pueda leer mas facil y se vea mas organizado, se limpia codigo

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -6,11 +6,15 @@ class UserObserver
 {
     public function created($user)
     {
-        logger()->info('se ha creado un usuario', ['name' => $user->name]);
+        logger()->info('se ha creado un usuario', [
+            'name' => $user->name
+        ]);
     }
 
     public function updated($user)
     {
-        logger()->info('se ha editado un usuario', ['name' => $user->name]);
+        logger()->info('se ha editado un usuario', [
+            'name' => $user->name
+        ]);
     }
 }
